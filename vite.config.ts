df5e1cbbf8e41824855e205d4a69bde0d48a2a2d
liuyong.wang.o <liuyonng.wang.o@nio.com>
2025-05-14 11:31:37 +0800

    更新 vite.config.ts，添加路径别名和代理配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@models": path.resolve(__dirname, "./public/models"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://your-backend.com",
+        changeOrigin: true,
+      },
+    },
+  },
+});
